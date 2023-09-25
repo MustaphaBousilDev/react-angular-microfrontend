fce06263ddfd36e4eb0227d2d8d5176883e66159
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-25 18:41:01 +0100

    Error handeling

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,32 +1,17 @@
-import React, {Suspense,useState} from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-//its for asynchronos loading
-const Header= React.lazy(() => import("home/Header"))
-const Footer= React.lazy(() => import("home/Footer"))
-
-const App = () => {
-  const [showHeader, setShowHeader]  = useState(false);
-  return (
-    <div className="text-3xl mx-auto max-w-6xl">
-      {
-        showHeader &&
-        (
-          <Suspense fallback={<div>Loading</div>}>
-            <Header/>
-          </Suspense>
-        )
-      }
-      <button 
-        onClick={()=> setShowHeader(true)}
-        className="text-3xl p-5">
-          Show the Header
-      </button>
-      <div className="my-10">Home Page Content</div>
-      <Suspense>
-        <Footer/>
-      </Suspense>
-    </div>
-  )
-};
+import Header from 'home/Header'
+import Footer from 'home/Footer'
+import SafeComponent from "./SafeComponent";
+ 
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <SafeComponent>
+      <Header/>
+    </SafeComponent>
+    <div className="my-10">Home Page Content</div>
+    <Footer/>
+  </div>
+);
 ReactDOM.render(<App />, document.getElementById("app"));
