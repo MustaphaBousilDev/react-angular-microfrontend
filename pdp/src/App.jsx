3cb6bca0f4b9b13511f3413205c18900698d1e17
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-25 18:05:46 +0100

    add diagramm for style architext

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {Suspense,useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import Header from 'home/Header'
-import Footer from 'home/Footer'
- 
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header/>
-    <div className="my-10">Home Page Content</div>
-    <Footer/>
-  </div>
-);
+//its for asynchronos loading
+const Header= React.lazy(() => import("home/Header"))
+const Footer= React.lazy(() => import("home/Footer"))
+
+const App = () => {
+  const [showHeader, setShowHeader]  = useState(false);
+  return (
+    <div className="text-3xl mx-auto max-w-6xl">
+      {
+        showHeader &&
+        (
+          <Suspense fallback={<div>Loading</div>}>
+            <Header/>
+          </Suspense>
+        )
+      }
+      <button 
+        onClick={()=> setShowHeader(true)}
+        className="text-3xl p-5">
+          Show the Header
+      </button>
+      <div className="my-10">Home Page Content</div>
+      <Suspense>
+        <Footer/>
+      </Suspense>
+    </div>
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
