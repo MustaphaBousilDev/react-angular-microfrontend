96bb13b47bf7cbc13332d368d703ee36b6d011d5
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-10-07 13:26:04 +0100

    finish cart shopping and start component add to cart

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,7 +1,8 @@
 import React, {useState,useEffect} from 'react'
 import { getProducts, currency } from './products'
-
+import { addToCart, useLoggedIn } from "cart/cart";
 export default function HomeContent(){
+    const loggedIn = useLoggedIn();
     const [products, setProducts] = useState([])
     useEffect(()=>{
         getProducts().then(setProducts)
@@ -11,7 +12,19 @@ export default function HomeContent(){
         <div className='my-10 grid grid-cols-4 gap-5'>
             {products.map((product)=>(
                 <div key={product.id}>
-                    <img src={product.image} alt={product.name} />
+                    <div><img src={product.image} alt={product.name} /></div>
+                    <div className='text-sm mt-4'>{product.description}</div>
+                    {loggedIn && (
+                        <div className="text-right mt-2">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                            onClick={() => addToCart(product.id)}
+                            id={`addtocart_${product.id}`}
+                        >
+                            Add to Cart
+                        </button>
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
